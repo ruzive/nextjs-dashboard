1ebc12391f32bffa9026ef2e937c92e63965d7a5
ruzive <ruzive@gmail.com>
2024-02-20 14:11:56 +0200

    1st commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,20 @@
+// import '@/app/ui/global.css';
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+import '@/app/ui/global.css';
+import { inter,lusitana } from '@/app/ui/fonts';
+
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
