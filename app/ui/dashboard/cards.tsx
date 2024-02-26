a4e010d5bb72ad8f5bf06aa09a661d5df8bee759
ruzive <ruzive@gmail.com>
2024-02-26 13:22:03 +0200

    cards enabled

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,7 +5,6 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -13,12 +12,13 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+
 export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+{/* 
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
